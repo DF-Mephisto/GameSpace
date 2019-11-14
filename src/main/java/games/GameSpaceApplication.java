84586c6af6249e16843124511c015388363c520f
Mephisto <mephist04@mail.ru>
2019-11-15 01:57:25 +0300

    Initial commit

---
+++ src/main/java/games/GameSpaceApplication.java
@@ -0,0 +1,14 @@
+package games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSpaceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GameSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/games/GameSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
