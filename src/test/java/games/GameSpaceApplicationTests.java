5372ad89f02d229cb9e6b52c7bb2eec706532785
Mephisto <mephist04@mail.ru>
2019-11-15 23:40:15 +0300

    Added home page

--- src/test/java/games/GameSpaceApplicationTests.java
+++ src/test/java/games/GameSpaceApplicationTests.java
@@ -1,5 +1,6 @@
 package games;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class GameSpaceApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
